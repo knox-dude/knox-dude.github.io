876994ce6ae9864f2e42a8c6dc947176ed2c291c
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-28 13:27:33 -0700

    tailwind config settings

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,7 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,tsx}"];
-export const theme = {
-  extend: {},
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: colors.neutral,
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        }
+      }
+    },
+  },
+  plugins: [],
 };
-export const mode = 'jit';
-export const plugins = [];
