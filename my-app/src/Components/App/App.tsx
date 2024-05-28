d035702b2c49ddd71ab779edd4b5dc11844d6e2a
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-28 13:10:28 -0700

    adding tailwind

--- my-app/src/Components/App/App.tsx
+++ my-app/src/Components/App/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter } from "react-router-dom"
 import NavBar from "@/Components/NavBar/NavBar"
 import AppRoutes from "@/Components/AppRoutes/AppRoutes"
+import "@/Components/App/App.css"
 
 function App() {
   return (
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,tsx}"];
+export const content = ["./src/**/*.{html,js,tsx}", "./index.html"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: '#e2e8f0',
+      secondary: '#4a5568',
+      tertiary: '#2d3748',
+      quaternary: '#1a202c',
+    },
+  },
 };
 export const mode = 'jit';
 export const plugins = [];
