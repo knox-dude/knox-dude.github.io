c845b97869b28318bd6e410db99732c658115123
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-29 16:55:21 -0700

    got project data from the json instead of the app component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,37 +2,7 @@ import NavBar from './NavBar'
 import ProjectList from './ProjectList'
 import About from './About'
 import BackgroundColor from './BackgroundColor'
-
-
-interface Project {
-  id: number
-  imagePath: string
-  description: string
-  link: string
-}
-
-const projects: Project[] = [
-  {
-    id: 1,
-    imagePath: 'photos/jeopardy-game.PNG',
-    description:
-      'A small app built in basic HTML/CSS/JS that lets you play jeopardy',
-    link: '/project1',
-  },
-  {
-    id: 2,
-    imagePath: 'photos/hacker-news-clone.PNG',
-    description:
-      'A clone of hacker news (https://hackernews.com) built with basic HTML/CSS/JS with all functionality of hacker news.',
-    link: '/project2',
-  },
-  {
-    id: 3,
-    imagePath: '/path/to/image3.jpg',
-    description: 'Project 3 Description',
-    link: '/project3',
-  },
-]
+import projects from "projects.json"
 
 function App() {
   return (
