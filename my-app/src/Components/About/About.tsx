2b86c9aee0e8dae0ca359cd2bb6c01851dbc98c5
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-28 19:24:39 -0700

    style changing

--- my-app/src/Components/About/About.tsx
+++ my-app/src/Components/About/About.tsx
@@ -5,7 +5,7 @@ function About() {
       <div>
         <h1 className=" text-4xl lg:text-6xl xl:text-9xl font-bold tracking-tight text-slate-700 ">Andrew Knox</h1>
         <h2 className="mt-3 text-lg lg:text-2xl xl:text-7xl font-medium tracking-tight text-slate-600">Junior Fullstack Dev</h2>
-        <p className="mt-4 max-w-sm l:max-w-md xl:text-2xl leading-normal">I am a junior developer who builds pleasing frontend interfaces and the APIs that interact with them. My tech stack includes Typescript, React, TailwindCSS, and more.</p>
+        <p className="mt-4 max-w-sm l:max-w-xl xl:max-w-2xl xl:text-2xl leading-normal">I am a junior developer who builds pleasing frontend interfaces and the APIs that interact with them. My tech stack includes Typescript, React, TailwindCSS, and more.</p>
       </div>
       <div className=" max-w-lg h-auto p-2 m-2">
         <img src="photos/andrew-photo.jpeg" alt="" />
