5955a289d65a2ecd54e594b2b576d159773987f4
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-29 17:21:59 -0700

    made project cards generally bigger

--- src/Components/ProjectList.tsx
+++ src/Components/ProjectList.tsx
@@ -15,7 +15,7 @@ const ProjectList = ({ projects }: ProjectListProps) => {
   return (
     <>
       <h1 className=" text text-center text-3xl ">Projects</h1>
-      <div className=" m-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className=" m-4 grid grid-cols-1 gap-4 sm:grid-cols-2">
         {projects.map((project) => (
           <ProjectCard
             key={project.id}
