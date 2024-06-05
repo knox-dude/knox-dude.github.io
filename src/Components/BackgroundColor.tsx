9e1238db4aacc55a2bb5b80e54e0e90759588cfa
Andrew Knox <andrew.knox.r@gmail.com>
2024-06-05 14:24:00 -0700

    change z index

--- src/Components/BackgroundColor.tsx
+++ src/Components/BackgroundColor.tsx
@@ -1,7 +1,7 @@
 function BackgroundColor() {
   return (
     <>
-      <div className=' fixed -z-10 h-1/2 top-1/2 w-full bg-light dark:bg-dark'></div>
+      <div className=' fixed -z-50 h-1/2 top-1/2 w-full bg-light dark:bg-dark'></div>
       {/* <figure className="bg-image"></figure> */}
     </>
   )
